9f3f49e7fa6c20f620f57a71bbb0f655882034cb
Igor Albuquerque <igor.cezar@ufrpe.br>
2019-05-22 19:41:05 -0300

    Modificações nas classes: venda, produto e cliente. Id retirado.

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,7 +1,7 @@
 class Venda < ApplicationRecord
   belongs_to :produto
-  #Entrará Cliente belongs_to :Cliente
-
+  belongs_to :cliente
+  has_many :produto
   validates :valorVenda, presence: { message: 'É um campo obrigatório' }
   validates :valorVenda, numericality: { message: 'É um campo apenas numérico' }
   validates :valorVenda, numericality: { greater_than_or_equal_to: 0, message: 'Deve ser maior ou igual a zero' }
--- db/migrate/20190522195359_create_produtos.rb
+++ db/migrate/20190522195359_create_produtos.rb
@@ -1,7 +1,7 @@
 class CreateProdutos < ActiveRecord::Migration[5.1]
   def change
     create_table :produtos do |t|
-      t.integer :id
+
       t.string :descricao
       t.decimal :precoCompra
       t.decimal :precoVenda
