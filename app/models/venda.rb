d7a4d4c58a19d5ce3ec0cb906d13f3759003cf11
Igor Albuquerque <igor.cezar@ufrpe.br>
2019-05-26 17:48:16 -0300

    Finalizado Vendas.

--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,2 +1,7 @@
 class Venda < ApplicationRecord
+  validates :produto, presence: { message: 'É um campo obrigatório' }
+  validates :produto, length: { minimum: 3, message: ' tem menos de 3 caracteres' }
+  validates :cliente, presence: { message: 'É um campo obrigatório' }
+  validates :cliente, length: { minimum: 3, message: ' tem menos de 3 caracteres' }
+  validates :quantidade, numericality: { greater_than_or_equal_to: 1, message: ' deve ser maior ou igual a um' }
 end
