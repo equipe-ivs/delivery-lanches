d0a089cd7f1fb480145a74419d3a075259d0f8c7
saucesar <sau.cesarlima2013@gmail.com>
2019-05-27 20:00:30 -0300

    Correcões em testes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190527204154) do
+ActiveRecord::Schema.define(version: 20190527052821) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "nome"
