641cc1cda2447fff268816dd15b2ecb6846e4761
saucesar <sau.cesarlima2013@gmail.com>
2019-06-03 19:56:33 -0300

    Relacionamento 1 para N entre Venda e produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,4 +1,5 @@
 class Produto < ApplicationRecord
   validates :descricao,presence:true ,length: {minimum: 5}
   validates :preco,presence: true,numericality: {message: "Insira apenas numeros"}
+  belongs_to :venda
 end
