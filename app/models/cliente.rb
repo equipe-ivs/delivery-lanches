7cc40027921bac970c6dea31e2b3f0b05dacd6a5
saucesar <sau.cesarlima2013@gmail.com>
2019-06-03 19:55:32 -0300

    Classe de model para endereço relacionando com cliente

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -2,6 +2,6 @@ class Cliente < ApplicationRecord
   validates :nome, presence: true,length: {minimum: 5}
   validates :cpf,presence: true,length: {minimum: 11,maximum: 11},numericality: {message: "Campo obrigatorio"}
   validates :telefone,presence: false,length:{minimum: 9}
-  validates :endereco,presence: true,length: {minimum: 5,message: "Campo obrigatorio"}
+  has_one :endereco
   has_many :vendas
 end
