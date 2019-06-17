f0a689e0a6334f2ec87739ee122759b2c424acc3
Igor Albuquerque <igor.cezar@ufrpe.br>
2019-06-17 20:14:48 -0300

    Campo CPF da Classe cliente.rb colocado como uniqueness.

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,6 +1,6 @@
 class Cliente < ApplicationRecord
   validates :nome, presence: true,length: {minimum: 5}
-  validates :cpf,presence: true,length: {minimum: 11,maximum: 11},numericality: {message: "Campo obrigatorio"}
+  validates :cpf,presence: true,length: {minimum: 11,maximum: 11},numericality: {message: "Campo obrigatorio"}, uniqueness: {message: 'CPF Já Consta no Sistema'}
   validates :telefone,presence: false,length:{minimum: 9}
   has_one :endereco
   has_many :vendas
