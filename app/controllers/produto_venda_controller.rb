dbe66b01cbcaa444e7dd7564782dede524357b9e
Sau Cesar <sau.cesarlima2013@gmail.com>
2019-06-18 21:28:10 -0300

    Conserto de issues apontados no codacy

--- app/controllers/produto_venda_controller.rb
+++ app/controllers/produto_venda_controller.rb
@@ -1,10 +1,10 @@
-class ProdutoVendaController < ApplicationController
+  class ProdutoVendaController < ApplicationController
   def new
     @produto_venda = Produto_venda.new
   end
 
   def create
-    @venda = Venda.find(params[:venda_id])
+    @venda = Venda.find_by_id(params[:venda_id])
     @produto_venda = @venda.produto_vendas.create(produto_venda_params)
     @produto_venda.preco_venda = Produto.find_by_id(@produto_venda.produto_id).preco
     @produto_venda.save
@@ -13,7 +13,7 @@ class ProdutoVendaController < ApplicationController
 
   def destroy
     @venda = Venda.find(params[:venda_id])
-    @produto_venda = ProdutoVenda.find(params[:id])
+    @produto_venda = ProdutoVenda.find_by_id(params[:id])
     @produto_venda.destroy
     redirect_to venda_path(@venda)
   end
