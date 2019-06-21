03b0d90b592d59b11472eefb00ce82353354ef4d
saucesar <sau.cesarlima2013@gmail.com>
2019-06-20 23:09:37 -0300

    adicionado configuracao do postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20190609192911) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clientes", force: :cascade do |t|
     t.string "nome"
     t.string "cpf"
